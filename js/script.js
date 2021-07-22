c33eeff7521ce9f9b4a2a876d4e7c1eb2d1aaab0
WCroW1997 <62266220+WCroW1997@users.noreply.github.com>
2021-07-22 19:01:11 +0300

    1
    pusk

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function viewDiv(){
+    document.getElementById("image1").style.display = "block";
+    document.getElementById("image2").style.display = "block";
+    document.getElementById("image3").style.display = "block";
+  };
